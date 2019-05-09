c36c81a44c85e8ba5a492449f3d161925734679c
Ben <ben_t44@bitbucket.com>
2019-05-09 10:05:39 -0700

    initial Django website

---
+++ web/benClient/witnessWebsite/src/witnessWebsite/pages/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# view of home page
+def home_view(request, *args, **kwargs):
+	print (args, kwargs)
+	print (request.user)
+
+	my_context = {
+		"title": "This is about us",
+		"this_is_true": True,
+		"my_number": 123,
+		"my_list": [123,4242, 312, 1234342, "ABC"],
+		"my_html": "<h1>Hello World</h1>"
+		}
+
+	return render(request, "home.html", my_context)
+
+from django.shortcuts import render
+
+# view of credits page
+def credits_view(request, *args, **kwargs):
+	print (args, kwargs)
+	print (request.user)
+	
+	return render(request, "credits.html", {})
+
+def index_view(request, *args, **kwargs):
+	print (args, kwargs)
+	print (request.user)
+	
+	return render(request, "index.html", {})
---
+++ web/benClient/witnessWebsite/src/witnessWebsite/witnessWebsite/urls.py
@@ -0,0 +1,33 @@
+"""witnessWebsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+# imports for pre-built bootstrap html pages
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+#my imports 
+from pages.views import * #all view pages
+
+urlpatterns = [
+	path('', home_view, name='home'),
+	path('index/', index_view, name='index'),
+	path('credits/', credits_view, name='credits'),
+    path('admin/', admin.site.urls),
+]
+
+# some balck magic for getting satic files to work!
+urlpatterns += staticfiles_urlpatterns()
